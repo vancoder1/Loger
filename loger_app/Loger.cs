3d1b3c3719169aca8ef681d836c4b3952319c664
vancoder1 <ivanzaporozhets25@gmail.com>
2021-06-22 16:23:01 +0300

    Create Loger class

---
+++ loger_app/Loger.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace loger_app
+{
+    class Loger
+    {
+        private string _path;
+
+        public string Path {
+            get
+            {
+                return _path;
+            }
+            set
+            {
+                _path = value;
+            }
+        }
+        public 
+    }
+}
